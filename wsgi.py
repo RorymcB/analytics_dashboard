7b4cf86ac3f51bc8b0d0d130cb5ab6eb3ed33003
RorymcB <ruairimcgb@gmail.com>
2025-03-02 19:20:39 +0100

    gunicorn error fixed - hopefully

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import server  # Import Flask server instance
+from app import server as app  # Import Flask server instance
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=8050, debug=True)
+    app.run(host="0.0.0.0", port=8050, debug=True)
